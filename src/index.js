69d76e38b07ae64a4f36bd37fa900c7f84677fec
rude <44342230+ruderude@users.noreply.github.com>
2021-03-12 08:55:47 +0900

    完了、削除ボタン実装

--- src/index.js
+++ src/index.js
@@ -15,28 +15,32 @@ const onClickAdd = () => {
   // buttonタグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグを未完了リストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグを未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-  console.log(li);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
-
-console.log("ttest");
-const func1 = () => {
-  console.log("area");
-};
-const test1 = 222;
-
-test1 && func1();
